619e9340678bd8f763c364231ddaaab0a025c2db
wuhao930406 <1148547900@qq.com>
2019-06-15 12:07:28 +0800

    yangyang

--- src/components/ScrollBar.js
+++ src/components/ScrollBar.js
@@ -1,14 +1,34 @@
 import FreeScrollBar from 'react-free-scrollbar';
+import React, { Component } from 'react';
 
-const ScrollBar = ({children}) => {
+class ScrollBar extends Component{
+  constructor(props){
+    super(props)
+    this.controlledScrollBar = null
+  }
+
+  shouldComponentUpdate(nextProps){
+    if(this.props.children!=nextProps.children){
+      if(this.controlledScrollBar){
+        setTimeout(()=>{
+          this.controlledScrollBar.setPosition({top: 1})
+        },100)
+      }
+      
+    }
+    return true
+  }
+
+  render(){
+    let {children} = this.props;
     return (
-      <FreeScrollBar autohide={true} timeout={1000}>
+      <FreeScrollBar start={"top"} ref={(el) => (this.controlledScrollBar = el)} autohide={true} timeout={1000}>
         {children}
       </FreeScrollBar>
     );
-  };
+  }
+}
+
+
   
-  ScrollBar.propTypes = {
-  };
-  
-  export default ScrollBar;
+export default ScrollBar;
